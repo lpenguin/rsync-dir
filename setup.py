ae7b82da467c75ec4dc427dde902fc22c2f842a8
nikita prianichnikov <lpenguin@yandex.ru>
2016-09-02 11:59:11 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='rsync-dir',
+    version='',
+    packages=['rsyncdir'],
+    url='',
+    license='',
+    author='nikita',
+    author_email='',
+    description=''
+)
